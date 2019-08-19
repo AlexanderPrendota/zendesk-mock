7d3f3338dac6f29c758711e93e1b630c004da1da
prendota <Prendota@mail.ru>
2019-08-19 14:48:14 +0300

    chore: init mock

---
+++ src/main/kotlin/com/zendesk/mock/zendesk/ZendesMockApplication.kt
@@ -0,0 +1,39 @@
+package com.zendesk.mock.zendesk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
+import kotlin.random.Random
+
+@SpringBootApplication
+class ZendeskApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZendeskApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/api/v2")
+class ZendeskApiRestController {
+    @GetMapping("/ticket_fields.json")
+    fun getCustomFields() = mapOf("ticket_fields" to emptyList<String>())
+
+    @GetMapping("/custom_roles.json")
+    fun getCustomRoles() = mapOf("custom_roles" to emptyList<String>())
+
+    @GetMapping("/users/{id}.json")
+    fun getUser(@PathVariable id: String) = mapOf("user" to User(Random.nextInt(), Random.nextInt().toString()))
+
+    @GetMapping("/ticket/{id}.json")
+    fun getTicket(@PathVariable id: Int) = mapOf("ticket" to Ticket(id))
+
+    @PutMapping("/tickets/{id}.json")
+    fun updateTicket(@PathVariable id: String) = mapOf("ticket" to Ticket(Random.nextInt()))
+
+    @PostMapping("/tickets.json")
+    fun createTicket(@RequestBody `object`: Any?) = mapOf("ticket" to Ticket(Random.nextInt()))
+
+    data class User(val id: Int, val name: String)
+    data class Ticket(val id: Int)
+}
+
---
+++ src/test/kotlin/com/zendesk/mock/zendesk/ZendeskApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.zendesk.mock.zendesk
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ZendeskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
